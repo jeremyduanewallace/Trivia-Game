d5cee77dd19cce1a167ec4784a363baf83397624
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-08-04 18:41:48 -0700

    character work

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,36 @@
-
 $(document).ready(function(){
   var counter = 0;
   var questions = [];
   var questionIndex = 0;
   var currentPlayer = '1';
+  var comedyQuestions = null;
+  var carQuestions = null;
+  var songQuestions = null;
+  var colorQuestions = null;
+  var moneyQuestions = null;
   
 
+
+  // append points
   function appendPoint(points){
     if(currentPlayer === '1'){
       var currentPoints = parseInt($('div.player1').html());
-      $('div.player1').html(currentPoints + points)
+      $('div.player1').html(currentPoints + points);
+      addPoints(currentPoints + points);
       currentPlayer = '2';
     }else if (currentPlayer === '2'){
-      $('div.player2').html(points);
+      var currentPoints = parseInt($('div.player2').html());
+      $('div.player2').html(currentPoints + points);
+      currentPlayer = '1'
+    }else{
+      currentPlayer = '1';
     }
   }
 
 
   // randomInt function
   function randomInt(){
-    return Math.floor(6 * Math.random());
+    return Math.floor(3 * Math.random());
   }
 
   
@@ -28,87 +39,162 @@ $(document).ready(function(){
     event.preventDefault();
     console.log('Checking answer');
     checkAnswer();
+    document.getElementById('answer_form').reset();
+    
   });
 
   
   // form validation check answer
   function checkAnswer(){
     var answer = $('#answer_input').val();
-    if(answer.toLowerCase() === questions[questionIndex].answer.toLowerCase()){
+    var actualAnswer = questions[questionIndex].answer.replace("-", " ");
+    if(answer.toLowerCase() === actualAnswer.toLowerCase()){
        appendPoint(questions[questionIndex].value)
        alert('correct');
     } else {
-      alert('wrong');
+       alert('wrong');
     }
   }
 
-
-  // get request trivia random question
-  $("button.random").click(function(data){
+  // add points
+  function addPoints(num){
+    if(num > 300){
+      //alert('wiiner')
+      $('#winner p').fadeIn(2000);
+    }
+  }
+    
+   
+    
+  // get request trivia comedy 
+  $("button.comedy").click(function(data){
     console.log('clicked');
-      $.getJSON("http://jservice.io/api/random?&count=20", function(result){
+    console.log(questionIndex);
+    if(comedyQuestions) {
+      console.log('comedy questions already exists');
+      questionIndex = randomInt();
+      $("div.card").html(comedyQuestions[questionIndex].question);
+      counter++
+      return;
+     }else{
+      $.getJSON("http://jservice.io/api/clues/?category=20", function(result){
         questions = result;
-        console.log('results', result);
-        $("div.card").html(result[0].question);
-        counter++
-      });
-  });
-
-
-
-   // get request trivia 100 points
-  $("button.one").click(function(data){
-    console.log('clicked');
-      $.getJSON("http://jservice.io/api/clues?&value=100", function(result){
-        questions = result;
+        comedyQuestions = result;
         questionIndex = randomInt();
         console.log('results', result);
         $("div.card").html(result[questionIndex].question);
         counter++
       });
+    }
   });
 
 
-  // get request trivia 200 points
-  $("button.two").click(function(data){
+
+   // get request trivia cars
+  $("button.car").click(function(data){
     console.log('clicked');
-      $.getJSON("http://jservice.io/api/clues?&value=200", function(result){
+    console.log(questionIndex);
+     if(carQuestions){
+      console.log('car question already exist');
+      questionIndex = randomInt(); 
+      $("div.card").html(carQuestions[questionIndex].question);
+      counter++
+      return;
+     }else{
+      $.getJSON("http://jservice.io/api/clues/?category=16", function(result){
         questions = result;
+        carQuestions = result;
         questionIndex = randomInt();
         console.log('results', result);
         $("div.card").html(result[questionIndex].question);
         counter++
       });
+     }
   });
 
 
-  // get request trivia 400 points
-  $("button.four").click(function(data){
+  // get request trivia silly songs
+  $("button.silly").click(function(data){
     console.log('clicked');
-      $.getJSON("http://jservice.io/api/clues?&value=400", function(result){
-        questions = result;
+    console.log(questionIndex);
+      if(songQuestions){
+        console.log('song questions already exist');
         questionIndex = randomInt();
-        console.log('results', result);
-        $("div.card").html(result[questionIndex].question);
+        $("div.card").html(songQuestions[questionIndex].question);
         counter++
-      });
+        return;
+      }else{
+        $.getJSON("http://jservice.io/api/clues/?category=27", function(result){
+          questions = result;
+          questionIndex = randomInt();
+          console.log('results', result);
+      if(result[questionIndex].question === ""){
+            result.splice(questionIndex, 4)
+          }
+          songQuestions = result;
+          $("div.card").html(result[questionIndex].question);
+          counter++
+        });
+      }
   });
 
 
-  // get request trivia 800 points
-  $("button.eight").click(function(data){
+  // get request trivia colors
+  $("button.color").click(function(data){
     console.log('clicked');
-      $.getJSON("http://jservice.io/api/clues?&value=800", function(result){
+    console.log(questionIndex)
+      if(colorQuestions){
+        console.log('color questoins already exist');
+        questionIndex = randomInt();
+        // console.log('results', result);
+       $("div.card").html(carQuestions[questionIndex].question);
+        counter++
+        return;
+      }else{
+        $.getJSON("http://jservice.io/api/clues/?category=36", function(result){
+          questions = result;
+          carQuestions = result;
+          questionIndex = randomInt();
+          console.log('results', result);
+          $("div.card").html(result[questionIndex].question);
+          counter++
+        });
+      }
+  });
+
+
+  // get request trivia money
+  $("button.money").click(function(data){
+    console.log('clicked');
+    console.log(questionIndex)
+    if(moneyQuestions){
+      console.log('money questions already exist');
+      questionsIndex = randomInt();
+      // console.log('results', result);
+      $("div.card").html(moneyQuestions[questionIndex].question);
+      counter++
+      return;
+    }else{
+      $.getJSON("http://jservice.io/api/clues/?category=76", function(result){
         questions = result;
-        questions = randomInt();
+        moneyQuestions = result;
+        questionsIndex = randomInt();
         console.log('results', result);
+        if(moneyQuestions[questionIndex].question === ""){
+          result.splice(questionIndex, 1)
+        }
         $("div.card").html(result[questionIndex].question);
         counter++
       });
+    }
   });
 
 
 
 }) 
+
+
+  
+
 
  
